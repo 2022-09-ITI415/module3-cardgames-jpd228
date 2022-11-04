6693c12b0f30add77ed738563a21ff26f544458f
jpd228 <113148088+jpd228@users.noreply.github.com>
2022-11-03 23:44:15 -0400

    More updates into the layouts script
    More code has been added to the layouts script. All of the code on page 666 has been added.

--- Assets/Prospector/__Scripts/Layouts.cs
+++ Assets/Prospector/__Scripts/Layouts.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class _Scripts : MonoBehaviour
+//The SlotDef class is not a subclass of MonoBehavior, so it doesn't need a seperate C# file.
+[System.Serializable] //This makes SlotsDefs visible in the Unity Inspector Pane
+public class SlotDef
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float x;
+    public float y;
+    public bool faceUp = false;
+    public string layerName = "Default";
+    public int layerID = 0;
+    public int = id;
+    public List<int> hiddenBy = new List<int>();
+    public string type = "slot";
+    public Vector2 stagger;
+}
 
-    // Update is called once per frame
-    void Update()
+public class Layout : MonoBehaviour
+{
+    public PT_XMLReader xmlr; //Just like the deck this has a PT_XMLReader
+    public PT_XMLHashtable xml;
+    public Vector2 multiplier;
+    //slotDef references
+    public List<SlotDef> slotDefs;
+    public SlotDef drawPile;
+    public SlotDef discardPile;
+    //This holds all of the possible names for the layers set by layerID
+    public string[] sortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" }
+
+    public void ReadLayout (string xmlText)
     {
-        
+        xmlr = new PT_XMLReader();
+        xmlr.Parse(xmlText);
+        xml = xmlr.xml["xml"][0];
+
+        //Read in the card multiplier, which sets card spacing
+        multiplier.x = float.Parse(xml["multiplier"][0].att("x"));
+        multiplier.y = float.Parse(xml["multiplier"][0].att("y"));
+
+        //read in the slots
+        SlotDef tSD;
+
+        PT_XMLHashlist slotsX = xml["slot"];
+
+        for (int i=0; i<slotsX.count; i++)
+        {
+            tSD = new SlotDef();
+            if (slotsX[i].HasAtt("type"))
+            {
+                tSD.type = slotsX[i].att("type");
+            } 
+            else
+            {
+                tSD.type = "slot";
+            }
+        }
+        tSD.x = float.Parse(slotsX[i].att("x"));
+        tSD.y = float.Parse(slotsX[i].att("y"));
+        tSD.layerID = int.Parse(slotsX[i].att("layer"));
+        tSD.layerName = sortingLayerNames[tSD.layerID];
     }
 }
