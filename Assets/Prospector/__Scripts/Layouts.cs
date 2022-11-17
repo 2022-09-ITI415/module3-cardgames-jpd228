ac7f917a76629badfb9b6cea7f43f669e850790c
jpd228 <113148088+jpd228@users.noreply.github.com>
2022-11-17 01:52:06 -0500

    Fixed some coding errors in Layouts
    had some syntax errors in the Layouts.cs script. There are still more errors to be fixed.

--- Assets/Prospector/__Scripts/Layouts.cs
+++ Assets/Prospector/__Scripts/Layouts.cs
@@ -11,7 +11,7 @@ public class SlotDef
     public bool faceUp = false;
     public string layerName = "Default";
     public int layerID = 0;
-    public int = id;
+    public int id;
     public List<int> hiddenBy = new List<int>();
     public string type = "slot";
     public Vector2 stagger;
@@ -27,9 +27,9 @@ public class Layout : MonoBehaviour
     public SlotDef drawPile;
     public SlotDef discardPile;
     //This holds all of the possible names for the layers set by layerID
-    public string[] sortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" }
+    public string[] sortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" };
 
-    public void ReadLayout (string xmlText)
+    public void ReadLayout(string xmlText)
     {
         xmlr = new PT_XMLReader();
         xmlr.Parse(xmlText);
@@ -44,45 +44,47 @@ public class Layout : MonoBehaviour
 
         PT_XMLHashlist slotsX = xml["slot"];
 
-        for (int i=0; i<slotsX.count; i++)
+        for (int i = 0; i < slotsX.count; i++)
         {
             tSD = new SlotDef();
             if (slotsX[i].HasAtt("type"))
             {
                 tSD.type = slotsX[i].att("type");
-            } 
+            }
             else
             {
                 tSD.type = "slot";
             }
-        }
-        tSD.x = float.Parse(slotsX[i].att("x"));
-        tSD.y = float.Parse(slotsX[i].att("y"));
-        tSD.layerID = int.Parse(slotsX[i].att("layer"));
-        tSD.layerName = sortingLayerNames[tSD.layerID];
-        switch (tSD.type)
-        {
-            case "slot":
-                tSD.faceUp = (slotsX[i].att("faceup") == "1");
-                tSD.id = int.Parse(slotsX[i].att("id"));
-                if (slotsX[i].HasAtt("hiddenby"))
-                {
-                    string[] hiding = slotsX[i].att("hiddenby").Split(',');
-                    foreach(string s in hiding)
-                    {
-                        tSD.hiddenBy.Add(int.Parse(s));
-                    }
-                }
-                slotDefs.Add(tSD);
-                break;
 
-            case "drawpile":
-                tSD.stagger.x = float.Parse(slotsX[i].att("xstagger"));
-                drawPile = tSD;
-                break;
-            case "discardpile":
-                discardPile = tSD;
-                break;
+            tSD.x = float.Parse(slotsX[i].att("x"));
+            tSD.y = float.Parse(slotsX[i].att("y"));
+            tSD.layerID = int.Parse(slotsX[i].att("layer"));
+            tSD.layerName = sortingLayerNames[tSD.layerID];
+
+            switch (tSD.type)
+            {
+                case "slot":
+                    tSD.faceUp = (slotsX[i].att("faceup") == "1");
+                    tSD.id = int.Parse(slotsX[i].att("id"));
+                    if (slotsX[i].HasAtt("hiddenby"))
+                    {
+                        string[] hiding = slotsX[i].att("hiddenby").Split(',');
+                        foreach (string s in hiding)
+                        {
+                            tSD.hiddenBy.Add(int.Parse(s));
+                        }
+                    }
+                    slotDefs.Add(tSD);
+                    break;
+
+                case "drawpile":
+                    tSD.stagger.x = float.Parse(slotsX[i].att("xstagger"));
+                    drawPile = tSD;
+                    break;
+                case "discardpile":
+                    discardPile = tSD;
+                    break;
+            }
         }
     }
 }
